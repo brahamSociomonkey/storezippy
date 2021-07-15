548e199144450db216b28f5ede497efd4856baf5
develperbrm <developer.brm@gmail.com>
2021-07-15 11:12:29 +0530

    changes

--- src/Components/Pages/Guide2.js
+++ src/Components/Pages/Guide2.js
@@ -19,7 +19,6 @@ const Guide2 = () => {
             Choosing the right product for online selling is an exhilarating yet
             challenging task. That's why we have created a free, downloadable
             guidebook on “Top 10 in-demand trending products” to help new
-            <br />
             aspiring eCommerce sellers launch their online store.
           </p>
 
@@ -30,10 +29,10 @@ const Guide2 = () => {
           </h2>
 
           <ul>
-            <li>What is order fulfillment and the steps involved.</li>
+            {/* <li>What is order fulfillment and the steps involved.</li>
             <li>Different order fulfillment options.</li>
             <li>Why outsource?</li>
-            <li>Important factors while choosing the right 3PL.</li>
+            <li>Important factors while choosing the right 3PL.</li> */}
             <li>Trending products for online selling</li>
             <li>Characteristics of best selling products</li>
             <li>The three step niche success validation process</li>
